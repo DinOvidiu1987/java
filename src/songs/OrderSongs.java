25cbf0aaa0c0e61fc53b88e1308126ac1a8dfe39
DinOvidiu1987 <ovidin1987@gmail.com>
2019-11-29 21:16:27 +0200

    exercitii

--- src/songs/OrderSongs.java
+++ src/songs/OrderSongs.java
@@ -4,19 +4,26 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeSet;
 
 public class OrderSongs {
     public static void main(String[] args) {
         try {
             BufferedReader file = new BufferedReader( new FileReader("songs.txt"));
             String line;
-            String[] songs = new String[10];
-            int i = 0;
+            TreeSet<String> songs = new TreeSet<>();
+
             while((line = file.readLine()) != null) {
-                songs[i] = line;
-                i++;
+                songs.add(line);
             }
-            System.out.println(songs.length);
+            System.out.println(songs.size());
+            System.out.println(songs);
+
+          //  Collections.shuffle(songs);
+            //System.out.println(songs);
+
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
